317c8a682026de662d5082ed8bf2c0633dabddd9
Ilham Wahabi <wahabiputra@gmail.com>
2019-10-30 05:59:53 +0700

    feat: Home - add explore button to header

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -10,13 +10,14 @@ const IndexPage = () => (
     <SEO title="Home" />
     <section className="home">
       <div className="header">
-        <div className="header_text">
+        <div className="header_wrapper">
           <p className="p_welcome">Selamat Datang di</p>
           <h1>Inkubator IT</h1>
           <p className="p_short-desc">
             Sebuah badan khusus dibawah <span>HMIF ITB</span> yang melayani pembuatan berbagai macam produk berbasis IT
             dan telah melayani puluhan klien selama <span>7 tahun</span> lebih.
           </p>
+          <button>Lebih Lanjut</button>
         </div>
       </div>
     </section>

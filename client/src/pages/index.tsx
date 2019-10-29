9fb1ee1a4efec49d4e0832741dc828837da0bdfc
Ilham Wahabi <wahabiputra@gmail.com>
2019-10-30 03:59:40 +0700

    feat: Home - initial Header text

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -3,12 +3,23 @@ import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
+import './scss/index.scss';
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to Inkubator IT.</p>
-    <p>Lets build something great.</p>
+    <section className="home">
+      <div className="header">
+        <div className="header_text">
+          <p className="p_welcome">Selamat Datang di</p>
+          <h1>Inkubator IT</h1>
+          <p className="p_short-desc">
+            Sebuah badan khusus dibawah <span>HMIF ITB</span> yang melayani pembuatan berbagai macam produk berbasis IT
+            dan telah melayani puluhan klien selama <span>7 tahun</span> lebih.
+          </p>
+        </div>
+      </div>
+    </section>
   </Layout>
 );
 

2b05b21701f99aa408a8d3943d1a1132ac761961
Ilham Wahabi <wahabiputra@gmail.com>
2019-10-30 13:19:45 +0700

    feat: Home - add typed.js to Works

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'gatsby';
+import Typed from 'typed.js';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
@@ -13,8 +14,8 @@ const IndexPage = () => {
         <p className="p_welcome">Selamat Datang di</p>
         <h1>Inkubator IT</h1>
         <p className="p_short-desc">
-          Badan usaha khusus dibawah <span>HMIF ITB</span> yang melayani pembuatan berbagai macam produk berbasis IT dan
-          telah melayani puluhan klien selama 7 tahun lebih.
+          Badan usaha khusus dibawah naungan <span>HMIF ITB</span> yang melayani pembuatan berbagai produk berbasis IT
+          dan telah melayani puluhan klien selama 7 tahun lebih.
         </p>
         <Link to="/about">
           <button>Lebih Lanjut</button>
@@ -23,16 +24,34 @@ const IndexPage = () => {
     </div>
   );
 
-  const renderWorks = () => (
-    <div className="works">
-      <div className="works-wrapper">
-        <h1>We Create...</h1>
-        <Link to="/portfolio">
-          <button>See Our Portfolio</button>
-        </Link>
+  useEffect(() => {
+    const typed = new Typed('.projects', {
+      strings: ['Web', 'Mobile', 'Desktop'],
+      typeSpeed: 70,
+      backSpeed: 40,
+      startDelay: 2000,
+      backDelay: 1000,
+      loop: true,
+      smartBackspace: true, // Default value
+    });
+
+    return () => typed.destroy();
+  });
+
+  const renderWorks = () => {
+    return (
+      <div className="works">
+        <div className="works-wrapper">
+          <h1>
+            Kami Melayani Pembuatan Aplikasi Berbasis <span className="projects"></span>
+          </h1>
+          <Link to="/portfolio">
+            <button>Lihat Portofolio Kami</button>
+          </Link>
+        </div>
       </div>
-    </div>
-  );
+    );
+  };
 
   return (
     <Layout>

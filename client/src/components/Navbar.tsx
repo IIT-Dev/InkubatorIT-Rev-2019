1a80a65010c1cc98ae43024cdfba4d5115c43d0e
Ilham Wahabi <wahabiputra@gmail.com>
2019-10-30 10:58:47 +0700

    feat: Navbar - add link to button

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 import './scss/Navbar.scss';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="logo">
+      <Link to="/" className="logo">
         <img src={require('../images/iit-logo-cropped.png')} alt="IIT Logo" />
         <div>
           <p className="title">Inkubator IT</p>
           <p className="subtitle">your IT-based solution</p>
         </div>
-      </div>
+      </Link>
       <div className="buttons">
-        <button>Home</button>
-        <button>About Us</button>
-        <button>Portofolio</button>
-        <button>Request</button>
+        <Link to="/">
+          <button>Home</button>
+        </Link>
+        <Link to="/about">
+          <button>About Us</button>
+        </Link>
+        <Link to="/portfolio">
+          <button>Portofolio</button>
+        </Link>
+        <Link to="/request">
+          <button className="request-btn">Request</button>
+        </Link>
       </div>
     </nav>
   );

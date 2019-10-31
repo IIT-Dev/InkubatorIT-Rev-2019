e2e5439402e5c5f51c0d8cc79762428ae0ce7265
Ilham Wahabi <wahabiputra@gmail.com>
2019-10-31 11:02:03 +0700

    ref: Portofolio - create separate section and handle icon size

--- client/src/pages/portofolio.tsx
+++ client/src/pages/portofolio.tsx
@@ -9,49 +9,63 @@ import { faChrome } from '@fortawesome/free-brands-svg-icons';
 import { faDesktop, faMobileAlt } from '@fortawesome/free-solid-svg-icons';
 
 const dummyProjects = [
-  { name: 'Aplikasi Web', url: 'https://via.placeholder.com/750/150', type: 'web' },
-  { name: 'Aplikasi Mobile', url: 'https://via.placeholder.com/750/150', type: 'mobile' },
-  { name: 'Aplikasi Desktop', url: 'https://via.placeholder.com/750/150', type: 'desktop' },
+  { name: 'Aplikasi Web', url: 'https://via.placeholder.com/750x500', type: 'web' },
+  { name: 'Aplikasi Mobile', url: 'https://via.placeholder.com/750x500', type: 'mobile' },
+  { name: 'Aplikasi Desktop', url: 'https://via.placeholder.com/750x500', type: 'desktop' },
 ];
 
 const Portfolio = () => {
-  const renderIcon = type => {
-    switch (type) {
-      case 'web':
-        return faChrome;
-      case 'desktop':
-        return faDesktop;
-      case 'mobile':
-        return faMobileAlt;
-    }
+  const title = (
+    <h1 className="section">
+      <span>Hasil Karya Kami</span>
+    </h1>
+  );
+
+  const projects = () => {
+    const renderIcon = type => {
+      switch (type) {
+        case 'web':
+          return faChrome;
+        case 'desktop':
+          return faDesktop;
+        case 'mobile':
+          return faMobileAlt;
+      }
+    };
+
+    return (
+      <div className="section projects">
+        {dummyProjects.map(project => (
+          <div className="project">
+            <div className="title">
+              <h3>{project.name}</h3>
+              <div>
+                <FontAwesomeIcon icon={renderIcon(project.type)} size="2x" className="icon" />
+              </div>
+            </div>
+            <div className="image">
+              <img src={project.url} alt={project.name} />
+            </div>
+          </div>
+        ))}
+      </div>
+    );
   };
+
+  const offer = (
+    <div className="section offer">
+      <h2>Ingin ide kamu direalisasikan juga?</h2>
+      <button>Request Disini</button>
+    </div>
+  );
 
   return (
     <Layout>
       <SEO title="Portofolio" />
       <section className="portofolio">
-        <h1 className="section">
-          <span>Hasil Karya Kami</span>
-        </h1>
-        <div className="section projects">
-          {dummyProjects.map(project => (
-            <div className="project">
-              <div className="title">
-                <h3>{project.name}</h3>
-                <div>
-                  <FontAwesomeIcon icon={renderIcon(project.type)} size="2x" className="icon" />
-                </div>
-              </div>
-              <div className="image">
-                <img src={project.url} alt={project.name} />
-              </div>
-            </div>
-          ))}
-        </div>
-        <div className="section">
-          <h2>Ingin ide kamu direalisasikan juga?</h2>
-          <button>Request Disini</button>
-        </div>
+        {title}
+        {projects()}
+        {offer}
       </section>
     </Layout>
   );

45ddbc5ff5b67098c390614eb769d4abd10b30c0
Ilham Wahabi <wahabiputra@gmail.com>
2019-10-31 16:57:22 +0700

    feat: Request - add text input field

--- client/src/pages/request.tsx
+++ client/src/pages/request.tsx
@@ -5,7 +5,48 @@ import './scss/request.scss';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
+const questions = [
+  {
+    label: 'Siapa nama Anda?',
+  },
+  {
+    label: 'Dari instansi mana Anda berasal?',
+  },
+  {
+    label: 'Apa jurusan Anda?',
+  },
+  {
+    label: 'Anda angkatan berapa?',
+  },
+  {
+    label: 'Apa nomor WhatsApp Anda?',
+  },
+  {
+    label: 'Apa ID Line Anda?',
+  },
+  {
+    label: 'Berapa ekspektasi biaya Anda untuk proyek ini?',
+  },
+];
+
 const Request = () => {
+  const InputField = props => {
+    const { label } = props;
+
+    return (
+      <div className="question">
+        <label htmlFor="name">{label}</label>
+        <input type="text" id="name" placeholder="Ketik jawaban disini..." autoFocus />
+        <div>
+          <button>OK</button>
+          <span>
+            atau tekan <span className="enter">ENTER</span>
+          </span>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <Layout>
       <SEO title="Request a Project" />
@@ -13,6 +54,9 @@ const Request = () => {
         <h1 className="title">
           <span>Formulir Pengajuan Proyek</span>
         </h1>
+        {questions.map((question, index) => (
+          <InputField key={index} {...question} />
+        ))}
       </section>
     </Layout>
   );

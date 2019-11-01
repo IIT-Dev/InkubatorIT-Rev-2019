f7e664e96470b07caefa1719c75bc1d009d880b1
Ilham Wahabi <wahabiputra@gmail.com>
2019-11-01 10:20:00 +0700

    fix: Login - use layout instead

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 import './scss/login.scss';
 
 import SEO from '../components/seo';
-import Footer from '../components/Footer';
+import Layout from '../components/layout';
 
 const Login = () => {
   return (
-    <>
+    <Layout>
       <SEO title="Login" />
       <section className="login">
         <form>
@@ -20,8 +20,7 @@ const Login = () => {
           <button>Login</button>
         </form>
       </section>
-      <Footer />
-    </>
+    </Layout>
   );
 };
 

125ac6622a2cbef39ad39d43d0030cb737516c68
Ilham Wahabi <wahabiputra@gmail.com>
2019-11-02 03:52:11 +0700

    ref: Home - create timeline elements data in separate file

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,22 +2,17 @@ import React, { useEffect } from 'react';
 import { Link } from 'gatsby';
 import Typed from 'typed.js';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faLaptop,
-  faCheck,
-  faHandshake,
-  faFileAlt,
-  faUserFriends,
-  faUserSecret,
-  faSearch,
-} from '@fortawesome/free-solid-svg-icons';
+import { faCheck, faFileAlt } from '@fortawesome/free-solid-svg-icons';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+
 import 'react-vertical-timeline-component/style.min.css';
+
+import './scss/index.scss';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-import './scss/index.scss';
+import { timelines } from '../data/home';
 
 const IndexPage = () => {
   const header = (
@@ -73,99 +68,79 @@ const IndexPage = () => {
     </div>
   );
 
+  const renderTimeLineElements = () => {
+    /* First element not included in data because we have a component in description */
+    const registerElements = (
+      <VerticalTimelineElement
+        visibilitySensorProps={{ offset: { bottom: 200 } }}
+        contentStyle={{
+          background: 'var(--secondary)',
+          color: '#fff',
+          boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.25)',
+        }}
+        contentArrowStyle={{ borderRight: '7px solid  var(--secondary)' }}
+        iconStyle={{ background: 'var(--secondary)', color: '#fff', boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.25)' }}
+        icon={<FontAwesomeIcon icon={faFileAlt} style={{ fontSize: '1.75em' }} />}
+      >
+        <h3 className="vertical-timeline-element-title">Mengisi Form Pengajuan Proyek</h3>
+        <p>
+          Pengisian dilakukan di{' '}
+          <Link to="/request" className="link">
+            laman ini
+          </Link>
+        </p>
+      </VerticalTimelineElement>
+    );
+
+    return (
+      <>
+        {registerElements}
+        {timelines.map((timeline, index) => {
+          let contentStyle: React.CSSProperties = {
+            borderTop: '5px solid var(--tertiary)',
+            boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.25)',
+          };
+          let iconStyle: React.CSSProperties = {
+            background: 'var(--tertiary)',
+            color: '#fff',
+            boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.25)',
+          };
+
+          if (timeline.isHighlighted) {
+            contentStyle = {
+              background: 'var(--secondary)',
+              color: '#fff',
+              boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.25)',
+            };
+
+            iconStyle = {
+              background: 'var(--secondary)',
+              color: '#fff',
+              boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.25)',
+            };
+          }
+
+          return (
+            <VerticalTimelineElement
+              key={index}
+              visibilitySensorProps={{ offset: { bottom: 200 } }}
+              contentStyle={contentStyle}
+              iconStyle={iconStyle}
+              icon={<FontAwesomeIcon icon={timeline.icon} style={{ fontSize: timeline.iconSize }} />}
+            >
+              <h3 className="vertical-timeline-element-title">{timeline.title}</h3>
+              <p dangerouslySetInnerHTML={{ __html: timeline.description }}></p>
+            </VerticalTimelineElement>
+          );
+        })}
+      </>
+    );
+  };
+
   const timeline = (
     <div className="timeline">
       <h2>Bagaimana cara kerjanya?</h2>
-      <VerticalTimeline>
-        <VerticalTimelineElement
-          visibilitySensorProps={{ offset: { bottom: 200 } }}
-          className="vertical-timeline-element--work"
-          contentStyle={{
-            background: 'var(--secondary)',
-            color: '#fff',
-            boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.25)',
-          }}
-          contentArrowStyle={{ borderRight: '7px solid  var(--secondary)' }}
-          iconStyle={{ background: 'var(--secondary)', color: '#fff', boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.25)' }}
-          icon={<FontAwesomeIcon icon={faFileAlt} style={{ fontSize: '1.75em' }} />}
-        >
-          <h3 className="vertical-timeline-element-title">Mengisi Form Pengajuan Proyek</h3>
-          <p>
-            Pengisian dilakukan di{' '}
-            <Link to="/request" className="link">
-              laman ini
-            </Link>
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          visibilitySensorProps={{ offset: { bottom: 200 } }}
-          className="vertical-timeline-element--work"
-          contentStyle={{ borderTop: '5px solid var(--tertiary)', boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.25)' }}
-          iconStyle={{ background: 'var(--tertiary)', color: '#fff', boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.25)' }}
-          icon={<FontAwesomeIcon icon={faUserFriends} style={{ fontSize: '1.25em' }} />}
-        >
-          <h3 className="vertical-timeline-element-title">Diskusi dengan Klien</h3>
-          <p>Tatap muka langsung antara manajer proyek dengan klien untuk diskusi seputar proyek yang diajukan</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          visibilitySensorProps={{ offset: { bottom: 200 } }}
-          className="vertical-timeline-element--work"
-          contentStyle={{ borderTop: '5px solid var(--tertiary)', boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.25)' }}
-          iconStyle={{ background: 'var(--tertiary)', color: '#fff', boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.25)' }}
-          icon={<FontAwesomeIcon icon={faSearch} style={{ fontSize: '1.25em' }} />}
-        >
-          <h3 className="vertical-timeline-element-title">Evaluasi Kelayakan Proyek</h3>
-          <p>Akan dipertimbangkan apakah proyek tersebut akan diterima atau tidak</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          visibilitySensorProps={{ offset: { bottom: 200 } }}
-          className="vertical-timeline-element--work"
-          contentStyle={{ borderTop: '5px solid var(--tertiary)', boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.25)' }}
-          iconStyle={{ background: 'var(--tertiary)', color: '#fff', boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.25)' }}
-          icon={<FontAwesomeIcon icon={faUserSecret} style={{ fontSize: '1.75em' }} />}
-        >
-          <h3 className="vertical-timeline-element-title">Mencari Developer</h3>
-          <p>Mencari developer yang akan mengerjakan dan merupakan mahasiswa HMIF ITB</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          visibilitySensorProps={{ offset: { bottom: 200 } }}
-          className="vertical-timeline-element--education"
-          contentStyle={{ borderTop: '5px solid var(--tertiary)', boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.25)' }}
-          iconStyle={{ background: 'var(--tertiary)', color: '#fff', boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.25)' }}
-          icon={<FontAwesomeIcon icon={faHandshake} style={{ fontSize: '1.25em' }} />}
-        >
-          <h3 className="vertical-timeline-element-title">Pengesahan MoU</h3>
-          <p>Penandatanganan spesifikasi proyek final dari klient</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          visibilitySensorProps={{ offset: { bottom: 200 } }}
-          className="vertical-timeline-element--education"
-          contentStyle={{ borderTop: '5px solid var(--tertiary)', boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.25)' }}
-          iconStyle={{ background: 'var(--tertiary)', color: '#fff', boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.25)' }}
-          icon={<FontAwesomeIcon icon={faLaptop} style={{ fontSize: '1.125em' }} />}
-        >
-          <h3 className="vertical-timeline-element-title">Pengerjaan Proyek</h3>
-          <p>Proyek dikerjakan oleh developer handal dan dikontrol oleh manajer proyek</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          visibilitySensorProps={{ offset: { bottom: 200 } }}
-          className="vertical-timeline-element--education"
-          contentStyle={{
-            background: 'var(--secondary)',
-            color: '#fff',
-            boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.25)',
-          }}
-          iconStyle={{
-            background: 'var(--secondary)',
-            color: '#fff',
-            boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.25)',
-          }}
-          icon={<FontAwesomeIcon icon={faCheck} style={{ fontSize: '1.5em' }} />}
-        >
-          <h3 className="vertical-timeline-element-title">Selesai</h3>
-          <p>Proyek selesai dikerjakan dan diserahkan kemudian penandatanganan selesai kontrak</p>
-        </VerticalTimelineElement>
-      </VerticalTimeline>
+      <VerticalTimeline>{renderTimeLineElements()}</VerticalTimeline>
     </div>
   );
 

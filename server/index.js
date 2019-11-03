0c63daf65d74dd3f6ba529eee8c2a40c676a30bb
Ilham Wahabi <wahabiputra@gmail.com>
2019-11-04 02:46:13 +0700

    init express

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(port, () => {
+  console.log(`Inkubator IT server is served at port ${port}`);
+});

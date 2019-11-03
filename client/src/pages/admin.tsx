69dc0495c57d6058eaedfde09aa2f7a7f65ef65d
Ilham Wahabi <wahabiputra@gmail.com>
2019-11-03 09:00:21 +0700

    feat: Admin - add buttons to people management

--- client/src/pages/admin.tsx
+++ client/src/pages/admin.tsx
@@ -102,6 +102,11 @@ const PeopleManagement = () => {
         <div className="img">
           <img src="https://via.placeholder.com/750x500" alt="Wildan Dicky Alnatara" />
         </div>
+        <div className="btn-group">
+          <button className="btn btn-add">Tambah</button>
+          <button className="btn btn-edit">Sunting</button>
+          <button className="btn btn-remove">Hapus</button>
+        </div>
       </div>
     </div>
   );

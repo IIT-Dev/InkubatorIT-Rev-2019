0675b2abfe02791acc28b97ce5fc8c95c72e4f06
Ilham Wahabi <wahabiputra@gmail.com>
2019-11-04 17:24:38 +0700

    feat: Portofolio - add react-select to filter projects

---
+++ client/src/data/portofolio.ts
@@ -0,0 +1,26 @@
+export const selectOptions = [
+  { value: 'web', label: 'Web' },
+  { value: 'mobile', label: 'Mobile' },
+  { value: 'desktop', label: 'Desktop' },
+];
+
+export const dummyProjects = [
+  {
+    name: 'Aplikasi Web',
+    url: 'https://via.placeholder.com/750x500',
+    type: 'web',
+    description: 'Aplikasi ini dapat dijalankan di web',
+  },
+  {
+    name: 'Aplikasi Mobile',
+    url: 'https://via.placeholder.com/750x500',
+    type: 'mobile',
+    description: 'Aplikasi ini dapat dijalankan di mobile',
+  },
+  {
+    name: 'Aplikasi Desktop',
+    url: 'https://via.placeholder.com/750x500',
+    type: 'desktop',
+    description: 'Aplikasi ini dapat dijalankan di desktop',
+  },
+];
--- client/src/pages/portofolio.tsx
+++ client/src/pages/portofolio.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'gatsby';
+import Select, { Styles } from 'react-select';
 
 import './scss/portofolio.scss';
 
@@ -7,33 +8,50 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 import Project from '../components/Project';
 
-const dummyProjects = [
-  {
-    name: 'Aplikasi Web',
-    url: 'https://via.placeholder.com/750x500',
-    type: 'web',
-    description: 'Aplikasi ini dapat dijalankan di web',
-  },
-  {
-    name: 'Aplikasi Mobile',
-    url: 'https://via.placeholder.com/750x500',
-    type: 'mobile',
-    description: 'Aplikasi ini dapat dijalankan di mobile',
-  },
-  {
-    name: 'Aplikasi Desktop',
-    url: 'https://via.placeholder.com/750x500',
-    type: 'desktop',
-    description: 'Aplikasi ini dapat dijalankan di desktop',
-  },
-];
+import { selectOptions, dummyProjects } from '../data/portofolio';
+
+const selectStyles: Styles = {
+  placeholder: base => ({ ...base, color: 'var(--tertiary)' }),
+  container: base => ({
+    ...base,
+    outline: 'none',
+  }),
+  control: base => ({
+    ...base,
+    border: '2px solid var(--tertiary)',
+    outline: 'none',
+    boxShadow: 'none',
+    ':hover': { border: '2px solid var(--tertiary)' },
+  }),
+  multiValue: base => ({ ...base, color: 'white', backgroundColor: 'var(--primary)' }),
+  multiValueLabel: base => ({ ...base, color: 'white', fontSize: 14 }),
+  multiValueRemove: base => ({ ...base, ':hover': { backgroundColor: 'var(--secondary)' } }),
+};
 
 const Portfolio = () => {
+  const [filters, setFilters] = useState();
+
   const title = (
     <h1 className="section">
       <span>Hasil Karya Kami</span>
     </h1>
   );
+
+  const filter = () => {
+    return (
+      <div className="section filters">
+        <h4>Platform: </h4>
+        <Select
+          value={filters}
+          onChange={selectedOption => setFilters(selectedOption)}
+          options={selectOptions}
+          isMulti
+          className="select"
+          styles={selectStyles}
+        />
+      </div>
+    );
+  };
 
   const projects = () => {
     return (
@@ -59,6 +77,7 @@ const Portfolio = () => {
       <SEO title="Portofolio" />
       <section className="portofolio">
         {title}
+        {filter()}
         {projects()}
         {offer}
       </section>

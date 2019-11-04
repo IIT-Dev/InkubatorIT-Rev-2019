a6b3cb3829f2f7482dfdb1c07fc2fb66833eaccf
Ilham Wahabi <wahabiputra@gmail.com>
2019-11-04 12:32:03 +0700

    feat: Request - show alert when form is submitted

--- client/src/pages/request.tsx
+++ client/src/pages/request.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import MaskedInput from 'react-text-mask';
 import emailMask from 'text-mask-addons/dist/emailMask';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
 
 import './scss/request.scss';
 
@@ -9,76 +11,98 @@ import SEO from '../components/seo';
 
 import { notices, questions } from '../data/request';
 
-const Request = () => {
-  const InputField = props => {
-    const { label, id, type, options } = props;
+const Alert = withReactContent(Swal);
 
-    const getField = () => {
-      switch (type) {
-        case 'text':
-          return <input type="text" id={id} name={id} placeholder="Ketik jawaban disini..." autoComplete="off" />;
-        case 'textarea':
-          return <textarea id={id} name={id} placeholder="Ketik jawaban disini..." rows={5} autoComplete="off" />;
-        case 'date':
-          return (
-            <MaskedInput
-              mask={[/\d/, /\d/, '/', /\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/]}
-              type="text"
-              id={id}
-              name={id}
-              placeholder="dd/mm/yyyy"
-              autoComplete="off"
-            />
-          );
-        case 'email':
-          return (
-            <MaskedInput
-              mask={emailMask}
-              type="text"
-              id={id}
-              name={id}
-              placeholder="example@gmail.com"
-              autoComplete="off"
-            />
-          );
-        case 'radio': {
-          return (
-            <div className={`options ${options.length > 2 && 'multiline'}`}>
-              {options.map((option, index) => (
-                <button key={index}>{option}</button>
-              ))}
-            </div>
-          );
-        }
+const InputField = props => {
+  const { label, id, type, options } = props;
+
+  const getField = () => {
+    switch (type) {
+      case 'text':
+        return <input type="text" id={id} name={id} placeholder="Ketik jawaban disini..." autoComplete="off" />;
+      case 'textarea':
+        return <textarea id={id} name={id} placeholder="Ketik jawaban disini..." rows={5} autoComplete="off" />;
+      case 'date':
+        return (
+          <MaskedInput
+            mask={[/\d/, /\d/, '/', /\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/]}
+            type="text"
+            id={id}
+            name={id}
+            placeholder="dd/mm/yyyy"
+            autoComplete="off"
+          />
+        );
+      case 'email':
+        return (
+          <MaskedInput
+            mask={emailMask}
+            type="text"
+            id={id}
+            name={id}
+            placeholder="example@gmail.com"
+            autoComplete="off"
+          />
+        );
+      case 'radio': {
+        return (
+          <div className={`options ${options.length > 2 && 'multiline'}`}>
+            {options.map((option, index) => (
+              <button key={index}>{option}</button>
+            ))}
+          </div>
+        );
       }
-    };
+    }
+  };
 
-    const getButton = () => {
-      if (type === 'radio') return;
-
-      return (
-        <div>
-          <button type="button">OK</button>
-          <span>
-            atau tekan <span className="enter">ENTER</span>
-          </span>
-        </div>
-      );
-    };
+  const getButton = () => {
+    if (type === 'radio') return;
 
     return (
-      <div className="question">
-        <label htmlFor={id}>{label}</label>
-        {getField()}
-        {getButton()}
+      <div>
+        <button type="button">OK</button>
+        <span>
+          atau tekan <span className="enter">ENTER</span>
+        </span>
       </div>
     );
   };
 
   return (
+    <div className="question">
+      <label htmlFor={id}>{label}</label>
+      {getField()}
+      {getButton()}
+    </div>
+  );
+};
+
+const Request = () => {
+  const actionSubmitForm = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const isSuccess = Math.random() > 0.5;
+    if (isSuccess) {
+      Alert.fire({
+        type: 'success',
+        title: 'Sip!',
+        text: `Proyek berhasil disubmit`,
+        footer: 'Tunggu kabar selanjutnya dari kami ya!',
+      });
+    } else {
+      Alert.fire({
+        type: 'error',
+        title: 'Oops...',
+        text: 'Terjadi kesalahan!',
+      });
+    }
+  };
+
+  return (
     <Layout>
       <SEO title="Request a Project" />
-      <form className="request" autoComplete="off" autoCorrect="off">
+      <form className="request" autoComplete="off" autoCorrect="off" onSubmit={actionSubmitForm}>
         <div className="title">
           <span>Formulir Pengajuan Proyek</span>
           {notices.map((notice, index) => (

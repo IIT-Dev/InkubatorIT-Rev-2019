9c7c310f2bb0a23ace878aa33813a357dadcaf17
Ilham Wahabi <wahabiputra@gmail.com>
2019-11-06 19:10:37 +0700

    fix: Portofolio - redesign projects, remove tooltip

--- client/src/components/Project.tsx
+++ client/src/components/Project.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChrome } from '@fortawesome/free-brands-svg-icons';
 import { faDesktop, faMobileAlt, faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
-import ReactTooltip from 'react-tooltip';
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
-import capitalize from 'lodash/capitalize';
 
 import './scss/Project.scss';
 
@@ -46,29 +44,26 @@ export const Project = props => {
 
     return (
       <div className="buttons">
-        <button className="btn edit-btn" data-tip="Sunting portofolio" data-for="portofolio-edit">
+        <button className="btn edit-btn">
           <FontAwesomeIcon icon={faEdit} color="var(--secondary)" />
         </button>
-        <ReactTooltip place="top" type="dark" effect="solid" id="portofolio-edit" />
-        <button className="btn remove-btn" data-tip="Hapus portofolio" data-for="portofolio-remove">
+        <button className="btn remove-btn">
           <FontAwesomeIcon icon={faTrash} color="var(--quartenary)" />
         </button>
-        <ReactTooltip place="top" type="dark" effect="solid" id="portofolio-remove" />
       </div>
     );
   };
 
   return (
     <div className="project" onClick={actionOpenAlert}>
-      <div className="title">
-        <h3>{name}</h3>
-        <div data-for="project-platform" data-tip={capitalize(type)}>
-          <FontAwesomeIcon icon={renderIcon(type)} size="2x" className="icon" />
-        </div>
-        <ReactTooltip place="top" type="dark" effect="solid" id="project-platform" />
-      </div>
       <div className="image">
         <img src={url} alt={name} />
+      </div>
+      <div className="title">
+        <h3>{name}</h3>
+        <div>
+          <FontAwesomeIcon icon={renderIcon(type)} className="icon" />
+        </div>
       </div>
       {renderAdminButton()}
     </div>

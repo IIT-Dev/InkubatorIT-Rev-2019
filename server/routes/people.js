aee273c5cd6967ac1f4a3fadd5e8dd770574daf9
Ilham Wahabi <wahabiputra@gmail.com>
2019-11-07 13:13:06 +0700

    feat: People - able to CRUD for

--- server/routes/people.js
+++ server/routes/people.js
@@ -1,57 +1,71 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const router = express.Router();
 
-const peoples = [
-  { id: 1, imageUrl: "", name: "Wildan Dicky", role: "" },
-  { id: 2, imageUrl: "", name: "Nabila Rahmi", role: "" },
-  { id: 3, imageUrl: "", name: "Cornelius", role: "" },
-  { id: 4, imageUrl: "", name: "Albert Sahala", role: "" }
-];
+const peopleSchema = new mongoose.Schema({
+  name: String,
+  role: String,
+  description: String,
+  imageUrl: String
+});
 
-router.get("/", (req, res) => {
+const People = mongoose.model("People", peopleSchema);
+
+router.get("/", async (req, res) => {
+  const peoples = await People.find();
   res.send(peoples);
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const { imageUrl, name, role } = req.body;
 
-  const people = {
-    id: peoples.length + 1,
-    imageUrl,
+  let people = new People({
     name,
-    role
-  };
+    role,
+    imageUrl
+  });
 
-  peoples.push(people);
-  res.send(peoples);
-});
-
-router.get("/:id", (req, res) => {
-  const people = peoples.find(c => c.id === parseInt(req.params.id));
-  if (!people)
-    return res.status(404).send("The people with given id not found");
-  res.send(people);
-});
-
-router.put("/:id", (req, res) => {
-  const people = peoples.find(people => people.id === parseInt(req.params.id));
-  if (!people)
-    return res.status(404).send("The people with given id not found");
-
-  people.name = req.body.name;
-  people.imageUrl = req.body.imageUrl;
-  people.role = req.body.role;
+  people = await people.save();
 
   res.send(people);
 });
 
-router.delete("/:id", (req, res) => {
-  const people = peoples.find(people => people.id === parseInt(req.params.id));
+router.get("/:id", async (req, res) => {
+  const people = await People.findById(req.params.id);
   if (!people)
-    return res.status(404).send("The people with given id not found");
+    return res.status(404).send("The people with given id is not exist");
 
-  const index = peoples.indexOf(people);
-  peoples.splice(index, 1);
+  res.send(people);
+});
+
+router.put("/:id", async (req, res) => {
+  let people = await People.findById(req.params.id);
+  if (!people)
+    return res.status(404).send("The people with given id is not exist");
+
+  const { imageUrl, name, role } = req.body;
+
+  people = await People.findByIdAndUpdate(
+    req.params.id,
+    {
+      name,
+      role,
+      imageUrl
+    },
+    { new: true }
+  );
+
+  if (!people)
+    return res.status(404).send("The people with the given ID was not found.");
+
+  res.send(people);
+});
+
+router.delete("/:id", async (req, res) => {
+  const people = await People.findByIdAndRemove(req.params.id);
+  if (!people)
+    return res.status(404).send("The people with given id is not exist");
 
   res.send(people);
 });

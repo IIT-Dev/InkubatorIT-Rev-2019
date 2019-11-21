ecbcfdac85fe6a50f59ea4649ccab51735f20dee
Ilham Wahabi <wahabiputra@gmail.com>
2019-11-22 02:14:55 +0700

    feat: Login - enable and navigate to admin if success

--- client/src/api/index.tsx
+++ client/src/api/index.tsx
@@ -1,20 +1,5 @@
 import axios from 'axios';
 
-import { IPortofolio } from '../interfaces/portofolio';
-
 export const api = axios.create({
   baseURL: 'http://localhost:8080',
 });
-
-export const fetchPortofolios = async setPortofolios => {
-  const response = await api.get('/portofolios');
-  setPortofolios(response.data);
-};
-
-export const editPortofolio = async (updatedPortofolio: IPortofolio) => {
-  await api.put(`/portofolios/${updatedPortofolio._id}`, updatedPortofolio);
-};
-
-export const deletePortofolio = async (_id: string) => {
-  await api.delete(`/portofolios/${_id}`);
-};
--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,22 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { navigate } from 'gatsby';
 
 import './scss/login.scss';
 
 import { SEO } from '../components/seo';
 import { Layout } from '../components/layout';
+import { api } from '../api';
 
 const Login = () => {
+  const [loginData, setLoginData] = useState({ username: '', password: '' });
+
+  const actionUpdateLoginField = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLoginData({ ...loginData, [event.target.id]: event.target.value });
+  };
+
+  const actionSubmitLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const response = await api.post('/user/login', loginData);
+      if (response) navigate('/admin');
+    } catch (e) {
+      alert(e);
+    }
+  };
+
   return (
     <Layout>
       <SEO title="Login" />
       <section className="login">
-        <form>
+        <form onSubmit={actionSubmitLogin}>
           <h4>Login</h4>
           <div className="field">
-            <input type="text" placeholder="username" autoFocus />
+            <input
+              type="text"
+              id="username"
+              placeholder="username"
+              autoFocus
+              value={loginData.username}
+              onChange={actionUpdateLoginField}
+            />
           </div>
           <div className="field">
-            <input type="password" placeholder="password" />
+            <input
+              type="password"
+              id="password"
+              placeholder="password"
+              value={loginData.password}
+              onChange={actionUpdateLoginField}
+            />
           </div>
           <button>Login</button>
         </form>

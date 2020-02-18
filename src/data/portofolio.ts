6cb3870635b6e6657c20174e73fa9ec6d7200e80
iwgx <wahabiputra@gmail.com>
2020-02-19 00:32:15 +0700

    fix: Portofolio - remove dummy data

--- src/data/portofolio.ts
+++ src/data/portofolio.ts
@@ -3,24 +3,3 @@ export const selectOptions = [
   { value: 'mobile', label: 'Mobile' },
   { value: 'desktop', label: 'Desktop' },
 ];
-
-export const dummyProjects = [
-  {
-    name: 'Aplikasi Web',
-    url: 'https://via.placeholder.com/750x500',
-    type: 'web',
-    description: 'Aplikasi ini dapat dijalankan di web',
-  },
-  {
-    name: 'Aplikasi Mobile',
-    url: 'https://via.placeholder.com/750x500',
-    type: 'mobile',
-    description: 'Aplikasi ini dapat dijalankan di mobile',
-  },
-  {
-    name: 'Aplikasi Desktop',
-    url: 'https://via.placeholder.com/750x500',
-    type: 'desktop',
-    description: 'Aplikasi ini dapat dijalankan di desktop',
-  },
-];

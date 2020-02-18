ccd178cc3bc1ead0d1d03894539d4105a377edc5
iwgx <wahabiputra@gmail.com>
2020-02-19 01:38:54 +0700

    feat: Login - add spinner when request

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -6,9 +6,11 @@ import './scss/login.scss';
 import { SEO } from '../components/seo';
 import { Layout } from '../components/layout';
 import { api } from '../api';
+import Spinner from '../components/Spinner';
 
 const Login = () => {
   const [loginData, setLoginData] = useState({ username: '', password: '' });
+  const [loading, setLoading] = useState(false);
 
   const actionUpdateLoginField = (event: React.ChangeEvent<HTMLInputElement>) => {
     setLoginData({ ...loginData, [event.target.id]: event.target.value });
@@ -17,13 +19,16 @@ const Login = () => {
   const actionSubmitLogin = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
+    setLoading(true);
     try {
       const response = await api.post('/user/login', loginData);
       if (response) {
         localStorage.setItem('iit:authenticated', 'true');
+        setLoading(false);
         navigate('/admin');
       }
     } catch (e) {
+      setLoading(false);
       alert(e);
     }
   };
@@ -31,6 +36,7 @@ const Login = () => {
   return (
     <Layout>
       <SEO title="Login" />
+      {loading && <Spinner />}
       <section className="login">
         <form onSubmit={actionSubmitLogin}>
           <h4>Login</h4>
